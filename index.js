bc7d19c45f2f2a4781149564e74567fd0229c152
mkhaliln <mkhalil@protonmail.com>
2020-10-06 09:23:23 +0530

    some codes optimized

--- index.js
+++ index.js
@@ -4,52 +4,53 @@ const fs = require('fs');
 
 const server = http.createServer((req, res)=>{
 
-let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
-// Extension of file
-let extname = path.extname(filePath);
+	let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+	
+	// Extension of file
+	let extname = path.extname(filePath);
 
-// Initial content type
-let contentType = 'text/html';
+	// Initial content type
+	let contentType = 'text/html';
 
-// Check extension and set content type
-switch(extname){
-	case '.js':
-		contentType = 'text/javascript';
-		break;
-	case '.css':
-		contentType = 'text/css';
-		break;
-	case '.json':
-		contentType = 'application/json';
-		break;
-	case '.png':
-		contentType = 'image/png';
-		break;
-	case '.jpg':
-		contentType = 'image/jpg';
-		break;
-}
+	// Check extension and set content type
+	switch(extname){
+		case '.js':
+			contentType = 'text/javascript';
+			break;
+		case '.css':
+			contentType = 'text/css';
+			break;
+		case '.json':
+			contentType = 'application/json';
+			break;
+		case '.png':
+			contentType = 'image/png';
+			break;
+		case '.jpg':
+			contentType = 'image/jpg';
+			break;
+	}
 
-// Read File
-fs.readFile(filePath, (err, content)=>{
-	if(err){
-		if(err.code == 'ENOENT'){
-			fs.readFile(path.join(__dirname, 'public' , '404.html'), (err, content) =>
-				{
-					res.writeHead(200, {'Content-Type': 'text/html'});
-					res.end(content, 'utf8');
-				});
+	// Read File
+	fs.readFile(filePath, (err, content)=>{
+		if(err){
+			if(err.code == 'ENOENT'){
+				fs.readFile(path.join(__dirname, 'public' , '404.html'), (err, content) =>
+					{
+						res.writeHead(200, {'Content-Type': 'text/html'});
+						res.end(content, 'utf8');
+					});
+			} else {
+				res.writeHead(500);
+				res.end(`Server Error: ${err.code}`);
+			}
 		} else {
-			res.writeHead(500);
-			res.end(`Server Error: ${err.code}`);
-		}
-	} else {
-		res.writeHead(200, {'Content-Type': contentType});
-		res.end(content, 'utf8');
-		}
+			res.writeHead(200, {'Content-Type': contentType});
+			res.end(content, 'utf8');
+			}
+		});
 	});
-});
-
+	
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, ()=> console.log(`Server is running on PORT ${PORT}`));
 
